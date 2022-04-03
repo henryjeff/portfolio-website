dbed2d355363ab8d8433f53e7c47ab83178f24e0
henryjeff <henryheffernan@gmail.com>
2022-04-03 15:40:41 -0400

    server updates

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ const path = require('path');
 const cors = require('cors');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 
 const app = express();
 const port = 8080;
@@ -16,42 +17,48 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-const nodemailer = require('nodemailer');
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    auth: {
-        user: process.env.FOLIO_EMAIL,
-        pass: process.env.FOLIO_PASSWORD,
-    },
-});
-
-transporter.verify().then(console.log).catch(console.error);
-
 // console.log(process.env.FOLIO_EMAIL);
 
 // Handle GET requests to /api route
 app.post('/api/send-email', (req, res) => {
     const { name, company, email, message } = req.body;
 
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        auth: {
+            user: process.env.FOLIO_EMAIL,
+            pass: process.env.FOLIO_PASSWORD,
+        },
+    });
+
     transporter
-        .sendMail({
-            from: '"Henry Heffernan" <henryheffernan.folio@gmail.com>', // sender address
-            to: 'henryheffernan@gmail.com, henryheffernan.folio@gmail.com', // list of receivers
-            subject: `Contact Request: ${name} <${email}> ${
-                company ? `from ${company}` : ''
-            } submitted a contact form`, // Subject line
-            text: `${message}`, // plain text body
-        })
-        .then((info) => {
-            console.log({ info });
-            res.json({ message: 'success' });
+        .verify()
+        .then(() => {
+            transporter
+                .sendMail({
+                    from: '"Henry Heffernan" <henryheffernan.folio@gmail.com>', // sender address
+                    to: 'henryheffernan@gmail.com, henryheffernan.folio@gmail.com', // list of receivers
+                    subject: `Contact Request: ${name} <${email}> ${
+                        company ? `from ${company}` : ''
+                    } submitted a contact form`, // Subject line
+                    text: `${message}`, // plain text body
+                })
+                .then((info) => {
+                    console.log({ info });
+                    res.json({ message: 'success' });
+                })
+                .catch((e) => {
+                    console.error(e);
+                    res.status(500).send(e);
+                });
         })
         .catch((e) => {
             console.error(e);
             res.status(500).send(e);
         });
 });
+
 // listen to app on port 8080
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
