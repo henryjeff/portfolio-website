24052467a1324e7e450d2dee214797d4de85005f
迷妹 <101113551+mimei1@users.noreply.github.com>
2022-11-12 11:31:00 +0800

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Handle GET requests to /api route
-app.post('/api/send-email', (req, res) => {
+app.post('/api/send-email', async (req, res) => {
     const { name, company, email, message } = req.body;
 
     const transporter = nodemailer.createTransport({
@@ -34,32 +34,47 @@ app.post('/api/send-email', (req, res) => {
         },
     });
 
-    transporter
-        .verify()
-        .then(() => {
-            transporter
-                .sendMail({
-                    from: `"${name}" <1919dotfun@gmail.com>`, // sender address
-                    to: '1919dotfun@gmail.com', // list of receivers
-                    subject: `${name} <${email}> ${
+    await new Promise((resolve, reject) => {
+        // verify connection configuration
+        transporter.verify(function (error, success) {
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                console.log("Server is ready to take our messages");
+                resolve(success);
+            }
+        });
+    });
+    
+    const mailData = {
+        from: {
+            name: `"${name}" <1919dotfun@gmail.com>`,
+            address: `"${email}"`,
+        },
+        replyTo: email,
+        to: "zaimakankanb@gmail.com",
+        subject: `${name} <${email}> ${
                         company ? `from ${company}` : ''
                     } 提交的表单`, // Subject line
-                    text: `${message}`, // plain text body
-                })
-                .then((info) => {
-                    console.log({ info });
-                    res.json({ message: 'success' });
-                })
-                .catch((e) => {
-                    console.error(e);
-                    res.status(500).send(e);
-                });
-        })
-        .catch((e) => {
-            console.error(e);
-            res.status(500).send(e);
+        text: `${message}`,
+    };
+    
+    await new Promise((resolve, reject) => {
+        // send mail
+        transporter.sendMail(mailData, (err, info) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            } else {
+                console.log(info);
+                resolve(info);
+            }
         });
-});
+    });
+    
+    res.status(200).json({ status: "OK" });
+    });
 
 // listen to app on port 8080
 app.listen(port, () => {
